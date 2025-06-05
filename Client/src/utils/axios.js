2cf03c4745bd846b7730e4b36a8de6b62f003423
Viraj Bipin Pradhan <134828338+VirajBP@users.noreply.github.com>
2025-06-05 13:03:52 +0530

    Axios file change

--- Client/src/utils/axios.js
+++ Client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: (process.env.REACT_APP_API_URL || 'http://localhost:5000/api').replace(/\/api$/, '') + '/api',
+    baseURL: (process.env.REACT_APP_API_URL || 'http://localhost:5000').replace(/\/api$/, ''),
     headers: {
         'Content-Type': 'application/json'
     },
