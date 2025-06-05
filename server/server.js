e33eedabcef067b63500e9794622a3df12f2005a
Viraj Bipin Pradhan <134828338+VirajBP@users.noreply.github.com>
2025-06-05 12:45:51 +0530

    Minor Changes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ require('./Model/Order');
 
 // Middleware
 app.use(cors({
-    origin: 'https://agri-connect-rho.vercel.app',
+    origin: 'https://agri-connect-gamma.vercel.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
