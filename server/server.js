5386602d5f4f6b76e36b46e1f15ca1d1c3f89c23
Viraj Bipin Pradhan <134828338+VirajBP@users.noreply.github.com>
2025-12-25 17:44:19 +0530

    Change origin of cors to the vercel link

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const loadDependencies = () => {
 
 // Setup CORS middleware immediately
 app.use(cors({
-    origin: 'https://agriconnect-frontend.vercel.app',
+    origin: 'https://agri-connect-gamma.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
